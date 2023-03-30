71dc1193ed7cdc88863add56f7c9fe8221181b68
istone <istoneyou1205@yahoo.co.jp>
2023-03-30 04:07:47 +0000

    リソースと画像の追加

--- aws-config.tf
+++ aws-config.tf
@@ -1,6 +1,6 @@
 resource "aws_config_configuration_recorder" "awsconfig" {
   name     = "config-recorder-${data.aws_caller_identity.current.account_id}"
-  role_arn = var.config_role
+  role_arn = aws_iam_policy.config.arn
 
   recording_group {
     all_supported                 = true
@@ -18,7 +18,7 @@ resource "aws_config_configuration_recorder_status" "awsconfig" {
 resource "aws_config_delivery_channel" "awsconfig" {
   name           = "awsconfig-${data.aws_caller_identity.current.account_id}"
   s3_bucket_name = aws_s3_bucket.security.id
-  depends_on     = ["aws_config_configuration_recorder.awsconfig"]
+  depends_on     = [aws_config_configuration_recorder.awsconfig]
   snapshot_delivery_properties {
     delivery_frequency = "One_Hour"
   }
--- guardduty.tf
+++ guardduty.tf
@@ -5,7 +5,7 @@ resource "aws_guardduty_detector" "guardduty" {
 resource "aws_guardduty_publishing_destination" "test" {
   detector_id     = aws_guardduty_detector.guardduty.id
   destination_arn = aws_s3_bucket.security.arn
-  kms_key_arn     = aws_kms_key.gd_key.arn
+  kms_key_arn     = aws_kms_key.key_s3.arn
 
   depends_on = [
     aws_s3_bucket_policy.bucket_policy,
